845a709b3aeead99187b2e2e9776e369f19bb106
itsoliviasparks <itsoliviasparks@gmail.com>
2022-12-19 21:27:31 -0500

    ✅✨

--- script.js
+++ script.js
@@ -7,59 +7,65 @@
     //when .random button is clicked, generate random color class to apply
     //only loop thru 8 color choices
 
+const rainbow = {};
+rainbow.clickCount = 0;
+rainbow.colorClassArr = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+rainbow.radioButton = document.querySelectorAll("input[type=radio]");
+rainbow.restartButton = document.querySelector('.restart');
 
-const colorClassArr = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+// get random item in given array
+rainbow.getRandomInArr = (arr) => {
+  const randomNum = Math.floor(Math.random() * arr.length);
+  return arr[randomNum];
+};
 
 //restart page
-const restartButton = document.querySelector('.restart');
-restartButton.addEventListener("click", function () {
+rainbow.restart = rainbow.restartButton.addEventListener("click", () => {
   window.location.reload();
 });
 
-const addRingToRainbow = (classToAdd, clickCount) => {
+//adding rings & cutout to rainbow
+rainbow.addRingToRainbow = (classToAdd, clickCount) => {
   const newRingDiv = document.createElement("div");
   newRingDiv.classList.add(`rainbow${clickCount}`, `${classToAdd}`);
   const rainbowContainer = document.querySelector(".rainbow-container");
   rainbowContainer.appendChild(newRingDiv);
 };
 
-//adding rings & cutout to rainbow
-const addRainbowCutout = () => {
+rainbow.addRainbowCutout = () => {
   const rainbowCutout = document.querySelector(".rainbow-cutout");
   rainbowCutout.classList.add("background");
 };
 
 //generating random value, if random was picked
 //setting the final value into variable matching color class names
-const findClassToAdd = () => {
-  if (userColorChoice === "random") {
-      classToAdd = getRandomInArr(colorClassArr);
+rainbow.findClassToAdd = () => {
+  if (rainbow.userColorChoice === "random") {
+      rainbow.classToAdd = rainbow.getRandomInArr(rainbow.colorClassArr);
   } else {
-      classToAdd = userColorChoice
+      rainbow.classToAdd = rainbow.userColorChoice
   }
-  addRainbowCutout();
-  addRingToRainbow(classToAdd, clickCount);
+  rainbow.addRainbowCutout();
+  rainbow.addRingToRainbow(rainbow.classToAdd, rainbow.clickCount);
 };
-
-getRandomInArr = (arr) => {
-  const randomNum = Math.floor(Math.random() * arr.length)
-  return arr[randomNum]
-};
-
 
 //getting value that user picked
 //& number of user clicks (limited to 8, b/c there are 8 rings on rainbow)
-const radioButton = document.querySelectorAll("input[type=radio]");
-let clickCount = 0;
-
-radioButton.forEach(radio => {
+rainbow.getUserChoiceAndClickCount = rainbow.radioButton.forEach(radio => {
   radio.addEventListener('click', function () {
-    userColorChoice = radio.value
-    if (clickCount < 8) {
-      clickCount++
+    rainbow.userColorChoice = radio.value
+    if (rainbow.clickCount < 8) {
+      rainbow.clickCount++
     } else {
-      clickCount = 1
+      rainbow.clickCount = 1
     };
-    findClassToAdd(userColorChoice);
+    rainbow.findClassToAdd(rainbow.userColorChoice);
   });
 });
+
+rainbow.init = () => {
+  rainbow.getUserChoiceAndClickCount();
+  rainbow.restart();
+};
+
+rainbow.init();
